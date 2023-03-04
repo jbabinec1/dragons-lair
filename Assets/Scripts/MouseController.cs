52a48426136d360a06d53c2d0ee01281f1adbf5f
Jared babinec <jbabinec1@gmail.com>
2023-03-04 18:21:38 -0500

    get pathfinding to work

--- Assets/Scripts/MouseController.cs
+++ Assets/Scripts/MouseController.cs
@@ -5,24 +5,82 @@ using UnityEngine;
 
 public class MouseController : MonoBehaviour
 {
+    public GameObject characterPrefab;
+    private CharacterInfo character;
+
+    private Pathfinder pathfinder;
+
+    public float speed;
+
+    private List<OverlayTile> path = new List<OverlayTile>();
+
+    public OverlayTile startingTile;
+    public OverlayTile activeTile;
+
+
     // Start is called before the first frame update
     void Start()
     {
+        //character = characterPrefab.GetComponent<CharacterInfo>();
+        pathfinder = new Pathfinder();
         
+        character = GameObject.Find("dragon_child").GetComponent<CharacterInfo>();
     }
 
+
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
 
         var focusedTileHit = GetFocusedOnTile();
 
-        if (focusedTileHit.HasValue)
-        {
-            GameObject tile_sprite = focusedTileHit.Value.collider.gameObject;
-            transform.position = tile_sprite.transform.position;
-            gameObject.GetComponent<SpriteRenderer>().sortingOrder= tile_sprite.GetComponent<SpriteRenderer>().sortingOrder;
+        var focusedOnTileFromPlayer = GetFocusedOnTileFromPlayer();
 
+        if (focusedOnTileFromPlayer.HasValue)
+        {
+            
+            character.startingTile = focusedOnTileFromPlayer.Value.collider.gameObject.GetComponent<OverlayTile>();
+
+        }
+
+        if (focusedTileHit != null && focusedTileHit.HasValue)
+        {
+            OverlayTile overlayTile = focusedTileHit.Value.collider.gameObject.GetComponent<OverlayTile>();
+
+            if (overlayTile != null)
+            {
+                transform.position = overlayTile.transform.position;
+                gameObject.GetComponent<SpriteRenderer>().sortingOrder = overlayTile.GetComponent<SpriteRenderer>().sortingOrder;
+                if (Input.GetMouseButtonDown(0) && overlayTile != null)
+                {
+                    if (character.activeTile == null)
+                    {
+                        // Set startingTile to the tile that the player is currently on
+                        character.startingTile = focusedOnTileFromPlayer?.collider.gameObject.GetComponent<OverlayTile>();
+
+                        path = pathfinder.FindPath(character.startingTile, overlayTile);
+
+                        character.activeTile = character.startingTile;
+
+                    }
+
+                    else
+                    {
+                        path = pathfinder.FindPath(character.activeTile, overlayTile);
+
+                    }
+
+                }
+
+            } 
+
+
+        }
+
+        if(path != null && path.Count > 0)
+        {
+            MoveAlongPath();
+            
         }
         
     }
@@ -41,4 +99,54 @@ public class MouseController : MonoBehaviour
 
         return null;
     }
+
+
+    private void PositionCharacterOnTile(OverlayTile tile)
+    {
+        character.transform.position = new Vector3(tile.transform.position.x, tile.transform.position.y, tile.transform.position.z);
+
+        character.GetComponent<SpriteRenderer>().sortingOrder = tile.GetComponent<SpriteRenderer>().sortingOrder;
+
+        character.activeTile = tile;
+
+        
+         character.startingTile = tile;
+
+     
+    }
+
+
+    public void MoveAlongPath()
+    {
+      
+        var step = speed * Time.deltaTime;
+
+        var zIndex = path[0].transform.position.z;
+
+        character.transform.position = Vector2.MoveTowards(character.transform.position, path[0].transform.position, step);
+
+        character.transform.position = new Vector3(character.transform.position.x, character.transform.position.y, zIndex);
+
+        if(Vector2.Distance(character.transform.position, path[0].transform.position) < 0.0001f)
+        {
+            PositionCharacterOnTile(path[0]);
+            path.RemoveAt(0);
+        }
+
+    }
+
+
+
+    public RaycastHit2D? GetFocusedOnTileFromPlayer()
+    {
+        RaycastHit2D[] hits = Physics2D.RaycastAll(character.transform.position, Vector2.zero);
+        // Debug.Log(hits);
+        if (hits.Length > 0)
+        {
+            return hits.OrderByDescending(i => i.collider.transform.position.z).First();
+        }
+
+        return null;
+    }
+
 }
---
+++ Assets/Scripts/Pathfinder.cs
@@ -0,0 +1,143 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+[Serializable]
+public class Pathfinder
+{
+
+    public List<OverlayTile> FindPath(OverlayTile start, OverlayTile end)
+    {
+
+        List<OverlayTile> openList = new List<OverlayTile>();
+
+        List<OverlayTile> closedList = new List<OverlayTile>();
+
+        openList.Add(start);
+
+        while (openList.Count > 0)
+        {
+            Debug.Log(openList.Count);
+
+            OverlayTile currentOverlayTile = openList.OrderBy(x => x.F).First();
+
+
+            if (currentOverlayTile != null)
+            {
+                openList.Remove(currentOverlayTile);
+                closedList.Add(currentOverlayTile);
+
+            }
+
+            if (currentOverlayTile == end)
+            {
+                return GetFinishedList(start, end);
+            }
+
+            var neighborTiles = GetNeighborTiles(currentOverlayTile);
+
+            foreach (var neighbor in neighborTiles)
+            {
+                // 1 = jump height
+                if (neighbor.isBlocked || closedList.Contains(neighbor) || Mathf.Abs(currentOverlayTile.gridLocation.z - neighbor.gridLocation.z) > 1)
+                {
+                    continue;
+                }
+
+                neighbor.G = GetManhattanDistance(start, neighbor);
+                neighbor.H = GetManhattanDistance(end, neighbor);
+
+                neighbor.previous = currentOverlayTile;
+
+                // neighbor.poop = currentOverlayTile.transform.position;
+
+                if (!openList.Contains(neighbor))
+                {
+                    openList.Add(neighbor);
+                }
+            }
+
+        }
+
+        return new List<OverlayTile>();
+    }
+
+
+
+
+    public List<OverlayTile> GetFinishedList(OverlayTile start, OverlayTile end)
+    {
+        List<OverlayTile> finishedList = new List<OverlayTile>();
+
+        OverlayTile currentTile = end;
+
+        while (currentTile != start)
+        {
+            finishedList.Add(currentTile);
+            currentTile = currentTile.previous;
+        }
+        finishedList.Reverse(); //List was reversed upon looping through
+
+        return finishedList;
+    }
+
+    public int GetManhattanDistance(OverlayTile start, OverlayTile neighbor)
+    {
+        return Mathf.Abs(start.gridLocation.x - neighbor.gridLocation.x) + Mathf.Abs(start.gridLocation.y - neighbor.gridLocation.y);
+    }
+
+    public List<OverlayTile> GetNeighborTiles(OverlayTile currentOverlayTile)
+    {
+        var map = MapManager.Instance.map;
+
+        List<OverlayTile> neighbors = new List<OverlayTile>();
+
+        //Top neighbor
+        Vector2Int locationToCheck = new Vector2Int(
+            currentOverlayTile.gridLocation.x,
+            currentOverlayTile.gridLocation.y + 1);
+
+        if (map.ContainsKey(locationToCheck))
+        {
+            neighbors.Add(map[locationToCheck]);
+        }
+
+        //Bottom neighbor
+        locationToCheck = new Vector2Int(
+           currentOverlayTile.gridLocation.x,
+           currentOverlayTile.gridLocation.y - 1);
+
+        if (map.ContainsKey(locationToCheck))
+        {
+            neighbors.Add(map[locationToCheck]);
+        }
+
+        //Right neighbor
+        locationToCheck = new Vector2Int(
+           currentOverlayTile.gridLocation.x + 1,
+           currentOverlayTile.gridLocation.y);
+
+        if (map.ContainsKey(locationToCheck))
+        {
+            neighbors.Add(map[locationToCheck]);
+        }
+
+        //Left neighbor
+        locationToCheck = new Vector2Int(
+           currentOverlayTile.gridLocation.x - 1,
+           currentOverlayTile.gridLocation.y);
+
+        if (map.ContainsKey(locationToCheck))
+        {
+            neighbors.Add(map[locationToCheck]);
+        }
+
+        return neighbors;
+    }
+
+
+
+
+}
